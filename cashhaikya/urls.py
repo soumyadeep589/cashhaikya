ce7295a8ed58a20878fd4a08e86eb0e8ae64d056
Soumyadeep Mondal <soumyadeepmondal@Soumyadeeps-MacBook-Air.local>
2023-08-09 09:04:41 +0530

    add api to admin url

--- cashhaikya/urls.py
+++ cashhaikya/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from cashhaikya.routers import router
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/v1/user/", include("user.urls")),
     path("api/v1/", include(router.urls)),
 ]
